09fee54c83d95aa104ec1dcf800f01f38f5efbdd
Esra Adsoy <9017268@student.tcrmbo.nl>
2022-05-20 13:39:36 +0200

    angry birds

--- angry-birds/app.js
+++ angry-birds/app.js
@@ -1,1 +1,24 @@
 //Oefening angry-birds
+
+ 
+ const nameChanged = function(event) { 
+       console.log(event); 
+   } 
+
+    //. is voor class
+    //# is voor id
+    //niks is voor html-tags
+
+const bird = document.querySelector(".bird");
+
+let px = 0;
+
+addEventListener("click", function(){
+px = px + 50;
+
+bird.style.left = px + "px";
+
+
+//console.log("geklikt");
+})
+
